238f6dcdd31d08034d9a129d47e4ab83c9893930
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-08-29 13:59:09 -0700

    master  -  Augmented the framework to includ the placehoder for the validation log

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,15 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
+
+Given ('today is Sunday', function () {
+   // Insert logic here that turns the phrase above into concrete actions
+   return 'pending';
+});
+When ('I ask whether it\'s Friday yet', function () {
+   // Insert logic here that turns the phrase above into concrete actions
+   return 'pending';
+});
+Then ('I should be told {string}', function (string) {
+ // Insert logic here that turns the phrase above into concrete actions
+ return 'pending';
+});
